c8ccbb804546c00dca7544ee5e552de9ae6dc85e
notgurev <notgurev@gmail.com>
2020-09-15 00:29:25 +0300

    Ввод параметров

--- client/main.js
+++ client/main.js
@@ -1,25 +1,42 @@
 let
     Y = document.getElementById('y-text'),
     R = document.getElementById("r-buttons"),
-    X = document.getElementsByName("x-checkboxes")
+    X = document.getElementsByName("x-checkboxes"),
+    selectedButton,
+    selectedCheckbox;
 
-function checkY() {
-    let y = Y.value.trim(); // .replace(",", ".") ?
+function checkY(yInputElement) {
+    let y = yInputElement.value.trim().replace(",", ".")
     if (y === "") {
-        Y.setCustomValidity("Введите значение");
+        yInputElement.setCustomValidity("Введите значение");
         return false;
     } else if (!isFinite(y)) {
-        Y.setCustomValidity("Значение должно быть числом");
+        yInputElement.setCustomValidity("Значение должно быть числом");
         return false;
     } else if (y <= -5 || y > 3) {
-        Y.setCustomValidity("Число должно входить в диапазон (-5 ... 3)")
+        yInputElement.setCustomValidity("Число должно входить в диапазон (-5 ... 3)")
     }
 }
 
-function submit(e) {
-
+function changeX(element) {
+    if (element.checked) {
+        if (selectedCheckbox !== undefined) {
+            selectedCheckbox.checked = false;
+        }
+        selectedCheckbox = element;
+    } else {
+        selectedCheckbox = undefined;
+    }
 }
 
-function clear(e) {
+function changeR(element) {
+    if (selectedButton !== undefined) {
+        selectedButton.classList.remove("selected_button");
+    }
+    selectedButton = element;
+    element.classList.add("selected_button");
+}
+
+function submit(e) {
 
 }
